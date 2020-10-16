382e4a85b3ac131e6af858bf4ffdae2d7bb206f7
Wahed <arvenaloverz@gmail.com>
2020-10-16 08:11:02 +0700

    berhasil menggambar dengan line strip

--- main.js
+++ main.js
@@ -2,16 +2,26 @@ function main(){
     var canvas  = document.getElementById("myCanvas");
     var gl      = canvas.getContext("webgl");
 
-    var vertexShaderSource = `
-    void main() {
-        gl_PointSize = 100.0;
-        gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
-    }`;
+    // Definisi data vertex 3 buah titik
+    /**
+     * Titik A (-0.5, -0.5)
+     * Titik B (0.5, -0.5)
+     * Titik C (0.5, 0.5)
+     */
+    var vertices = [
+        -0.5, -0.5, //Titik A
+        0.5, -0.5, //Titik B
+        0.5, 0.5, //Titik C
+        -0.5, -0.5 //Titik A
+    ];
 
-    var fragmentShaderSource = `
-    void main() {
-        gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
-    }`;
+    var vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+    var vertexShaderSource = document.getElementById("vertexShaderSource").text;
+    var fragmentShaderSource = document.getElementById("fragmentShaderSource").text;
 
     // Buat .c di GPU
     var vertexShader    = gl.createShader(gl.VERTEX_SHADER);
@@ -36,8 +46,25 @@ function main(){
     // Mulai menggunakan konteks (cat)
     gl.useProgram(shaderProgram);
 
+    // Ajarkan atribute a_Position di GPU 
+    // tentang pengambilan data vertex dari ARRAY_BUFFER
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    var aPositionLoc = gl.getAttribLocation(shaderProgram, "a_Position");
+    gl.vertexAttribPointer(
+        aPositionLoc, 
+        2, 
+        gl.FLOAT, 
+        false, 
+        0, 
+        0);
+    gl.enableVertexAttribArray(aPositionLoc);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
     
-    gl.drawArrays(gl.POINTS, 0, 1);
+    var primitive = gl.LINE_STRIP;
+    var offset = 0;
+    var nVertex = 6;
+    gl.drawArrays(primitive, offset, nVertex);
 }

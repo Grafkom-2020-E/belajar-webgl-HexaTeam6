198853288430ec6b7344ce652782d03169df19db
Wahed <arvenaloverz@gmail.com>
2020-10-16 08:13:11 +0700

    berhasil menggambar denga line loop

--- main.js
+++ main.js
@@ -12,7 +12,6 @@ function main(){
         -0.5, -0.5, //Titik A
         0.5, -0.5, //Titik B
         0.5, 0.5, //Titik C
-        -0.5, -0.5 //Titik A
     ];
 
     var vertexBuffer = gl.createBuffer();
@@ -63,7 +62,7 @@ function main(){
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
     
-    var primitive = gl.LINE_STRIP;
+    var primitive = gl.LINE_LOOP;
     var offset = 0;
     var nVertex = 4;
     gl.drawArrays(primitive, offset, nVertex);

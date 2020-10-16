e946db0276a3795e9f5138438adf9ba6f65cca5d
Wahed <arvenaloverz@gmail.com>
2020-10-16 09:00:52 +0700

    menggambar 1 segitiga monokrom + 1 segitiga gradasi

--- main.js
+++ main.js
@@ -10,11 +10,13 @@ function main(){
      * Titik D (-0.5, 0.5)
      */
     var vertices = [
-        0.5, -0.5, //Titik B
-        -0.5, -0.5, //Titik A
-        0.5, 0.5, //Titik C
-        -0.5, 0.5, //Titik A
-    ];
+        -0.5, -0.5, 1.0, 0.0, 0.0,      // Titik A, MERAH
+        0.5, -0.5, 0.0, 1.0, 0.0,       // Titik B, HIJAU
+        0.5, 0.5, 0.0, 0.0, 1.0,        // Titik C, BIRU
+        -0.5, -0.5, 1.0, 1.0, 1.0,      // Titik A, PUTIH
+        0.5, 0.5, 1.0, 1.0, 1.0,        // Titik C, PUTIH
+        -0.5, 0.5, 1.0, 1.0, 1.0        // Titik D, PUTIH
+      ];
 
     var vertexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
@@ -50,23 +52,32 @@ function main(){
     // Ajarkan atribute a_Position di GPU 
     // tentang pengambilan data vertex dari ARRAY_BUFFER
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    var aPositionLoc = gl.getAttribLocation(shaderProgram, "a_Position");
-    gl.vertexAttribPointer(
-        aPositionLoc, 
-        2, 
-        gl.FLOAT, 
-        false, 
-        0, 
-        0);
-    gl.enableVertexAttribArray(aPositionLoc);
-    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+  var aPositionLoc = gl.getAttribLocation(shaderProgram, "a_Position");
+  var aColorLoc = gl.getAttribLocation(shaderProgram, "a_Color");
+  gl.vertexAttribPointer(
+    aPositionLoc, 
+    2, 
+    gl.FLOAT, 
+    false, 
+    5 * Float32Array.BYTES_PER_ELEMENT, 
+    0);
+  gl.vertexAttribPointer(
+    aColorLoc, 
+    3, 
+    gl.FLOAT, 
+    false, 
+    5 * Float32Array.BYTES_PER_ELEMENT, 
+    2 * Float32Array.BYTES_PER_ELEMENT);
+  gl.enableVertexAttribArray(aPositionLoc);
+  gl.enableVertexAttribArray(aColorLoc);
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
     gl.viewport(100, 0, 600, 600);
     
-    var primitive = gl.TRIANGLE_STRIP;
+    var primitive = gl.TRIANGLES;
     var offset = 0;
-    var nVertex = 4;
+    var nVertex = 6;
     gl.drawArrays(primitive, offset, nVertex);
 }
